907457229da863d93311d7e9e44db02f9ae66f26
Dharmadeep <dharmadeepmadisetty@gmail.com>
2024-11-24 23:05:12 +0530

    ADawd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FetchDataService } from './fetch-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'exp8';
+  userList: any[] = [];
+
+  constructor(private fetchService: FetchDataService) {}
+  ngOnInit(): void {
+    this.fetchService.getUser().subscribe((users) => {
+      console.log(users); // Check the structure of the API response
+      this.userList = users;
+    });
+  }
 }
